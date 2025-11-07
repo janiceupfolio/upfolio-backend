af920587150d2a42ab171b5e02d7fc00b0962f52
Urmil Parsaniya <urmilp@intuz.net>
2025-11-07 18:34:46 +0530

    File upload size increase

--- routes/index.ts
+++ routes/index.ts
@@ -23,7 +23,7 @@ const storage = multer.memoryStorage();
 
 const upload = multer({
   storage: storage,
-  limits: { fileSize: 10 * 1024 * 1024 }, // Limit file size to 10 MB
+  limits: { fileSize: 50 * 1024 * 1024 }, // Limit file size to 50 MB
 });
 
 export default (app: Application): void => {
